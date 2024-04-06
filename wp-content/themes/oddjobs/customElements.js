b1afbf0159d3ac224def5acfa6fc054b50bf6683
leap0489 <leap0489@users.noreply.github.com>
2024-04-06 00:23:30 +0000

    deploy: e8df960b053d9b5536958b93bd3a85ea827cca2b

--- wp-content/themes/oddjobs/customElements.js
+++ wp-content/themes/oddjobs/customElements.js
@@ -5,3 +5,11 @@ class CopyrightYear extends HTMLElement{
 }
 
 customElements.define("x-year", CopyrightYear);
+
+class TwoSidedMarket extends HTMLElement{
+    connectedCallback(){
+        this.innerHTML = `<a href="shop">Browse listings</a>&nbsp;<a href="job-provider-application">List jobs</a>`;
+    }
+}
+
+customElements.define("x-twosides", TwoSidedMarket);
